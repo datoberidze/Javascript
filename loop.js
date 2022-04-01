79c4c0369b56bd53fbaab49fbe1d983ea539a9fa
datoberidze <datoberidze1999@gmail.com>
2022-04-01 22:15:25 +0400

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,63 @@
+
+// დავალება 1
+
+
+//let user = {
+  //  firstname: 'Giorgi',
+    //lastname: 'Smith',
+    //age: 25,
+    //studentstatus: 'Active',
+//};
+
+//console.log(user.studentstatus);
+
+// დავალება 2
+
+ //let Davit = [ 'Davit', 'Beridze', 23, 1999  ];
+
+ //for (let x of Davit){
+   //  console.log(x)};
+
+ // let x = 0;
+  //while (x < Davit.length ){
+    //  console.log(Davit[x]);
+      //x++;};
+
+// დავალება 3
+
+//let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+//for ( let x of numbers) {if(x > 5){console.log(x)}};
+
+//დავალება 4
+
+//let user = {
+  //  name: 'Giorgi',
+   // age: 20, 
+   // studentstatus: 'active'};
+
+//if (user.age < 18)(console.log('Hello'))
+//else if(user.name == 'Giorgi')(console.log('Hello Giorgi'))
+//else if(user.studentstatus == 'active' || user.age < 25)(console.log('Hello world'))
+//else (console.log('Error'));
+
+//დავალება 5
+
+//let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'Pineapple'];
+
+//for (let x of array) {if (typeof x === 'string'  )
+//(console.log(x))};
+
+//დავალება 6
+
+//let array = [[2, -3, 5, 10], [25, -24, -11, 100],[-6, -7, 10]];
+
+//for (let x of array)
+//for (let y of x)
+//if (y > 0)(console.log(y));
+
+// დავალება 7
+
+//let array = [32, 10,'hello', null, 'hello2', 50 ]
+
+//for (let x of array) if( x % 5 == 0  )(console.log(x))
