d957a4bc7c957d72c2684acd5e588b540d7d7ece
datoberidze <datoberidze1999@gmail.com>
2022-04-12 00:53:21 +0400

    Create Homework2.js

---
+++ Homework2.js
@@ -0,0 +1,78 @@
+//დავალება 1
+
+// function numbers(n) {
+
+//     let sum = 0; 
+    
+//     for (let i = 0; i < n.length; i++) {
+
+//     if (n[i] < 1) continue;
+//     sum += n[i]
+// }
+        
+//     return sum;
+//     }
+
+// const res = numbers([2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8]);
+
+// console.log(res)
+
+//დავალება 2
+
+// function numbers(n) {
+
+//     let sum = 0;
+
+//     for (let i = 0; i < n.length; i++) { 
+
+     
+//      sum += n[i] 
+//     }
+
+//     return sum;
+// }
+
+//    const res = numbers([10, 50, 6, 7, 8, 11, 6, 3, 9]);
+
+//    console.log(res)
+
+//დავალება 3
+
+
+    // function result(user){
+    //     let sum = 0;
+    //     for(let i = 0; i < user.length; i++){
+    //         if(isloggedin == true){
+    //             return firstname + " " + lastname
+    //         }else{
+    //             return "false";
+    //         }
+    //     }
+    // }
+
+    // const res = result(
+    //     firstname = 'Giorgi',
+    //     lastname = 'Saakadze',
+    //     age = 32,
+    //     isloggedin = true
+    // )
+
+    // console.log(res)
+
+    //დავალება4
+
+     function maximum(n){
+         let x = 0;
+
+         for (let i = 0; i < n.length; i++){
+
+             return n;
+
+         }
+     }
+
+    const sum = [1, 2, 3];
+
+    const max = Math.max(...sum);
+
+    console.log(max)
