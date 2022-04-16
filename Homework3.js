917b03be9c36712a420173ef3c6c2c736083047c
datoberidze <datoberidze1999@gmail.com>
2022-04-16 16:55:40 +0400

    Update Homework3.js

---
+++ Homework3.js
@@ -0,0 +1,90 @@
+// დავალება 1
+// let array = [5, 25, 89, 120, 36]
+
+// array.push("Javascript", "python");
+
+// array.unshift("Html", "CSS");
+
+// array.shift();
+
+// array.pop();
+
+// console.log(array);
+
+// დავალება 2
+
+// let array = ["orange", "banana", "pear"];
+
+// console.log("Before " + array.length);
+
+// array.push("Apple", "Pineapple")
+
+// array.unshift("Watermelon");
+
+// console.log("Afrer 1st Quantity = " + array.length)
+
+// array.splice(2, 0, "Mango")
+
+// array.pop()
+
+// array.shift()
+
+// console.log("Last Quantity = " + array.length);
+
+// დავალება 3
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let result = array.map(item => item / 3)
+
+// console.log(result);
+
+// დავალება 4
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let result = array.filter(item => typeof item === 'number')
+
+// console.log(result);
+
+// დავალება 5
+
+// let languages = ['html', 'css', 'javascript', 'pyton', 'php'];
+
+// let result = languages.filter( item => item.length > 3);
+
+// console.log(result);
+
+// დავალება 6
+
+// let array = ['academy ', 'of ', 'digital ', 'industries'].reduce(function(accumulator,currentValue){
+//     return accumulator.concat(currentValue);
+// },
+// ""
+// );
+
+// console.log(array);  კითხვა მაქვს ???
+
+// დავალება 7
+
+// let array = [12, " google ", 32, null, "yahoo", 25];
+
+// let result = array.map((item) => {
+//   if (typeof item === "number") {
+//     return Math.pow(item, 2);
+//   } else if (typeof item === "string") {
+//     return item.toLocaleUpperCase();
+//   } else if (typeof item === "object") {
+//     return item;
+//   }
+// });
+
+// console.log(result);
+
+//დავალება 8
+
+// let words = ["Madrid", "Rome", "Milan", "Berlin"];
+
+// let result = words.filter((item) => item.includes("m") || item.includes("M"));
+
+// console.log(result);
